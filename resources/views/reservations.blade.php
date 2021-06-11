1e341d93518098e404c3af21efa814c90aafe0cb
zer0xday <majcher10@gmail.com>
2021-06-11 16:13:37 +0200

    Rm: commented code

--- resources/views/reservations.blade.php
+++ resources/views/reservations.blade.php
@@ -19,13 +19,5 @@
                 @endforeach
             </tbody>
         </table>
-{{--        <ul class="collection with-header black">--}}
-{{--            <li class="collection-header"><h3 style="color: black">Dokonane rezerwacje</h3></li>--}}
-{{--            @foreach($customers as $customer)--}}
-{{--                <li class="collection-item">--}}
-{{--                    {{ $customer->full_name }} - zarezerwowane miejsca: {{ $customer->customersPlaces->count() }}--}}
-{{--                </li>--}}
-{{--            @endforeach--}}
-{{--        </ul>--}}
     </section>
 @endsection
